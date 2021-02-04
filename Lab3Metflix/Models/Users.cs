b04d998ffbc31562eb75c9c5f1eb5bd16f449cee
Eden <edennyk@gmail.com>
2021-02-03 20:06:37 -0500

    added all files

---
+++ Lab3Metflix/Models/Users.cs
@@ -0,0 +1,17 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+
+
+namespace Lab3Metflix.Models
+{
+    [DynamoDBTable("Users")]
+    public class Users
+    {
+        [DynamoDBHashKey]
+        public string Email { get; set; }
+        [DynamoDBProperty]
+        public string UserName { get; set; }
+        [DynamoDBProperty]
+        public string Password { get; set; }
+
+    }
+}
